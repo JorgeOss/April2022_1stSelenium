d7abdf5bdf0c1eea434d9d8e0714da9c02d97103
JorgeOss <jorge788oss@gmail.com>
2022-08-29 12:16:56 -0500

    1st commit

---
+++ src/launchBrowser/LauchChrome.java
@@ -0,0 +1,32 @@
+package launchBrowser;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LauchChrome {
+	
+	public static void main(String[] args) throws InterruptedException{
+			
+		System.setProperty("WebDriver.Chrome.Driver", "C:\\Users\\Jorge Rios\\Selenium\\1st_Selenium\\driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		
+		
+		
+		
+		
+		
+		
+
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+
+}
